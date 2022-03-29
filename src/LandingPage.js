bbc22280611bab26115067fc72e661a7ac72f62a
GiovanneGuerra <giovanneguerra@hotmail.com>
2022-03-29 13:21:41 +0100

    refactor: Include the right text in FAQ

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './assets/sadu-logo.png';
-import unique from './assets/unique.png';
 import tutorial from './assets/tutorial.png';
 import Accordion from 'react-bootstrap/Accordion';
 import Carousel from 'react-bootstrap/Carousel';
@@ -163,37 +162,25 @@ function LandingPage(){
                 <Accordion.Item eventKey="0">
                 <Accordion.Header>I connected the right wallet to the app but it shows that my NFTree belongs to a different address. Why?</Accordion.Header>
                 <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                    est laborum.
+                    Substrate account addresses (Polkadot, Kusama etc.) may look different on different networks but they have all the same private key underneath. You can see all transformations of any address on <a href="https://polkadot.subscan.io/tools/ss58_transform" target="_blank">https://polkadot.subscan.io/tools/ss58_transform</a>
                 </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
-                    <Accordion.Header>Whom can I contact if I have questions regarding the marketplace?</Accordion.Header>
+                    <Accordion.Header>I see my NTF on the My Gallery page twice and one of them is “on hold”</Accordion.Header>
                     <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                    est laborum.
+                        It can happen if the previous version of the market had information about an unfinished listing. In that case: (1) Go to the page of ‘on hold’ token and complete listing. (2) Then delist this token.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
-                    <Accordion.Header>How can I get KSM to my account?</Accordion.Header>
+                    <Accordion.Header>I see the error “1010: Invalid Transaction: Inability to pay some fees, e.g. account balance too low”</Accordion.Header>
                     <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                    est laborum.
+                        Just wait for half a minute and try again.
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                    <Accordion.Header>Whom can I contact if I have questions regarding the marketplace?</Accordion.Header>
+                    <Accordion.Body>
+                        Please contact <a className="link" href="mailto:info@sadu.io">info@sadu.io</a> if you have any questions.
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
